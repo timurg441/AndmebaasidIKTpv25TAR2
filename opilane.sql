36c80aea98a598351a8689a07431bce330d67dd0
Timur <1ame.xyz07@gmail.com>
2026-02-02 16:05:12 +0200

    .

--- opilane.sql
+++ opilane.sql
@@ -5,7 +5,7 @@ use timurSQL;
 CREATE TABLE opilane(
 opilaneID int PRIMARY KEY identity(1,1), 
 eesnimi varchar(25),
-perenimi varchar(30) NOT null,
+perenimi varchar(30) NOT null UNIQUE,
 synniaeg date,
 aadress TEXT,
 kas_opib bit);
@@ -14,3 +14,10 @@ SELECT * FROM opilane;
 
 --tabeli kustutamine
 DROP TABLE opilane;
+
+--lisamine 3.kirjet korraga
+INSERT INTO opilane(eesnimi, perenimi, synniaeg, kas_opib)
+VALUES ('Nikita', 'Petrov', '2023-12-12', 1),
+('Nikita', 'Alekseev', '2020-12-12', 1),
+('Nikita', 'Nikitin', '2021-12-12', 1);
+Select * from opilane; 
